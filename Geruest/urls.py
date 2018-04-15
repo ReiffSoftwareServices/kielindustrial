305602c7b30e44de8dc10e0d16dab100aaa0c3a6
Jan <info@movie-match.com>
2018-04-15 23:00:33 +0200

    view created for index

--- Geruest/urls.py
+++ Geruest/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import  url
 from django.urls import path, include
-
+from . import views
 
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls)
+	path ('',views.index, name="index"),
+    path(r'admin/', admin.site.urls),
     #url(r'^', include('books.urls'))
 ]
